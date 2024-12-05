8db32c1f39b46f29b3cd211403c77797bdfaee0a
Yannik <dieschlafmuetze@gmail.com>
2024-12-05 18:20:03 +0100

    remove .idea folder and move testset to test resources.

--- Dominating Set Verifier/src/test/java/de/uni/bremen/ds/VerifierTest.java
+++ Dominating Set Verifier/src/test/java/de/uni/bremen/ds/VerifierTest.java
@@ -1,5 +1,7 @@
 package de.uni.bremen.ds;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import de.uni.bremen.ds.io.Importer;
 import de.uni.bremen.ds.model.Graph;
 import de.uni.bremen.ds.model.Solution;
@@ -8,32 +10,34 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class TestVerifier {
 
-    final static String TEST_DIR = "testset";
+  final static String TEST_DIR = "testset";
 
-    @Test
-    void testAll() {
-        Stream.of(Objects.requireNonNull(new File(TEST_DIR).listFiles()))
-                .filter(file -> !file.isDirectory())
-                .forEach(file -> {
-                    if(file.getName().endsWith(".gr")) {
-                        try {
-                            Importer importer = new Importer();
-                            Graph g = importer.readGraph(file.getPath());
-                            Solution sol = importer.readSolution(file.getPath().replace(".gr", ".sol"));
+  @Test
+  void testAll() throws URISyntaxException {
+    //Dirty test to verify the verifier
+    File testDir = new File(Objects.requireNonNull(getClass().getClassLoader().getResource(TEST_DIR)).toURI());
 
-                            DSVerifier verifier = new DSVerifier();
-                            assertTrue(verifier.verify(g, sol));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-    }
+    Stream.of(Objects.requireNonNull(testDir.listFiles()))
+        .filter(file -> !file.isDirectory())
+        .forEach(file -> {
+          if (file.getName().endsWith(".gr")) {
+            try {
+              Importer importer = new Importer();
+              Graph g = importer.readGraph(file.getPath());
+              Solution sol = importer.readSolution(file.getPath().replace(".gr", ".sol"));
+
+              DSVerifier verifier = new DSVerifier();
+              assertTrue(verifier.verify(g, sol));
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+        });
+  }
 }
